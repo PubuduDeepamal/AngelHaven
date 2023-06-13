a6f37fa04541605124a8fe1b731428c016d61f4d
PubuduDeepamal <pubudu@lpw.lk>
2023-06-14 04:54:23 +0530

    update

---
+++ src/views/product.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import heder1 from '@/components/heder1.vue'
+	import meal from '@/components/meal.vue'
+  import footersection from '@/components/footersection.vue'
+</script>
+
+<template>
+  <main>
+    <heder1/>
+    <meal/>
+    <footersection/>
+  </main>
+</template>
