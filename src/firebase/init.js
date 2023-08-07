157ccfbc7815585b416f28d54d90e62187495a8d
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-08 04:42:33 +0530

    Update init.js

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBr2T8Etmq_qcUEJ1LzgpRdrKVwwPeobJA",
-  authDomain: "angelhaven-3f27b.firebaseapp.com",
-  projectId: "angelhaven-3f27b",
-  storageBucket: "angelhaven-3f27b.appspot.com",
-  messagingSenderId: "191213082500",
-  appId: "1:191213082500:web:9f33f6cc781a939a7cefd1",
-  measurementId: "G-39S3CLD151"
+  apiKey: "AIzaSyBANo6uLaoGsboc9ex5R2HAOh1xHP3r9RI",
+  authDomain: "angelhaven-a68a5.firebaseapp.com",
+  databaseURL: "https://angelhaven-a68a5-default-rtdb.firebaseio.com",
+  projectId: "angelhaven-a68a5",
+  storageBucket: "angelhaven-a68a5.appspot.com",
+  messagingSenderId: "129958052805",
+  appId: "1:129958052805:web:a225b7f40d21fedb30c1ac"
 };
 
 const firbaseApp = initializeApp(firebaseConfig);
