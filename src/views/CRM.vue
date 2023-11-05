95abc456bd61aba60555c834d097a4250489336b
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-11-06 05:19:15 +0530

    Create CRM.vue

---
+++ src/views/CRM.vue
@@ -0,0 +1,13 @@
+<script setup>
+import heder3 from '@/components/heder3.vue'
+import crm from '@/components/crm.vue'
+import footersection from '@/components/footersection.vue'
+</script>
+
+<template>
+  <main>
+    <heder3/>
+    <crm/>
+    <footersection/>
+  </main>
+</template>
